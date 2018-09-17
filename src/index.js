697c0e9a5702030153ea85d96dc24fb336589b71
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-09-16 23:59:13 -0500

    removed some unneeded items

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
